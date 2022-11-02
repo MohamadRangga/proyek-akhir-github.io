0de7c21ab6dfc7cd5233a4e36e54717f86233d43
MohamadRangga <200914021@fellow.lpkia.ac.id>
2022-11-02 15:36:45 +0700

    templating

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -1,1 +1,14 @@
-<h1> Ini View Index </h1>
+@extends('layouts.main')
+
+@section('content-first')
+
+    <div class="card mt-2">
+        <div class="card-header">
+            Person
+        </div>
+
+        <div class="card-body">
+            Selamat Jow
+        </div>
+    </div>
+@endsection
